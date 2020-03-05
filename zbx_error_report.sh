d9e7c47558284024b6bc190d0a6f8fe4ea912d9e
VladimirKosyuk <42845051+VladimirKosyuk@users.noreply.github.com>
2020-03-05 10:21:53 +0300

    Update zbx_error_report.sh

--- zbx_error_report.sh
+++ zbx_error_report.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mailto=example@contoso.com
 ERR=$(cat /var/log/zabbix/zabbix_*.log | grep `date +"20%y%m%d"` | grep "FATAL\|failed" | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }' | sort | uniq -c | sort -nk1 -r)
 if [ -n "$ERR" ]; then
