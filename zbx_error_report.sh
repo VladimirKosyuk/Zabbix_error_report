c8eab639c55289b077b26e227957797458c4f51a
VladimirKosyuk <42845051+VladimirKosyuk@users.noreply.github.com>
2020-03-05 10:11:24 +0300

    Create zbx_error_report.sh
    •	Collects all zabbix errors
    •	Find all that is for today
    •	Count repeated errors
    •	If found some then send an email

---
+++ zbx_error_report.sh
@@ -0,0 +1,5 @@
+mailto=example@contoso.com
+ERR=$(cat /var/log/zabbix/zabbix_*.log | grep `date +"20%y%m%d"` | grep "FATAL\|failed" | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }' | sort | uniq -c | sort -nk1 -r)
+if [ -n "$ERR" ]; then
+echo  "$ERR\n"| mail -v -s "$HOSTNAME zabbix error" $mailto
+fi
